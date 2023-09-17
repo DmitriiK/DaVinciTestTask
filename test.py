caadced155ab14be1c2272b1f366a7e99b50295e
DmitriiK <dklmn@mail.ru>
2023-09-18 00:39:19 +0300

    tests for github actions

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-from tests.tests import test_save_df
+# from tests.tests import test_save_df
+
+def dummy_test_passed():
+    assert True
+
+
+def dummy_test_failed():
+    assert False
+
 
 if __name__ == "__main__":
     # test_read_config()
     # test_read_config, test_aggr_df, test_join_df
-    test_save_df()
+    # test_save_df()
+    dummy_test_passed()
