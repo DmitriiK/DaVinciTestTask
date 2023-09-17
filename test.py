56e51e7064ec374256790e033d5984a001aba1f7
DmitriiK <dklmn@mail.ru>
2023-09-18 00:56:50 +0300

    test pytest

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
 # from tests.tests import test_save_df
 
-def dummy_test_passed():
+def test_passed_dummy():
     assert True
     print ('passed')
 
 
-def dummy_test_failed():
+def test_failed_dummy():
     assert False, 'shit happens'
 
 
-if __name__ == "__main__":
-    # test_read_config()
-    # test_read_config, test_aggr_df, test_join_df
-    # test_save_df()
-    dummy_test_passed()
