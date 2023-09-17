3858be7dbb77a39052208f821c71a6e66f5a79ed
DmitriiK <dklmn@mail.ru>
2023-09-18 02:43:00 +0300

    returned to ubuntu for tests and skipped dummy fail test

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-from tests.automation_testing import test_read_orders_df
+import pytest
 import pandas as pd
+
+from tests.automation_testing import test_read_orders_df
 
 
 def test_passed_dummy():
@@ -7,6 +9,7 @@ def test_passed_dummy():
     print('passed')
 
 
+@pytest.mark.skip(reason="this test was make to ensure that fails of test work")
 def test_failed_dummy():
     assert False, 'shit happens'
 
