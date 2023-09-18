dd883b087d3ef97ea9d1825fc2f2f8aadbfc7038
DmitriiK <dklmn@mail.ru>
2023-09-18 03:07:28 +0300

    prints in pytest output

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 import pytest
-import pandas as pd
+from os import path
 
-from tests.automation_testing import test_read_orders_df
+import etl_modules.load_source_data as lsd
+from config_modules.metadata_classes import EntityTypes
+
+
+def test_read_orders_df():
+    et = EntityTypes.order
+    fn = path.join("tests", "input_data", "olist_orders_dataset.csv")
+    df = lsd.get_pandas_dataframe(et, fn)
+    print(f'{df.shape=} got')
+    expected = (24, 5)
+    ass_mess = f'{df.shape =} is not correct, expected {expected}'
+    assert df.shape == expected, ass_mess
+    # 24 rows, 5 columns
 
 
 def test_passed_dummy():
@@ -12,15 +24,3 @@ def test_passed_dummy():
 @pytest.mark.skip(reason="test was make to ensure that fails of test work")
 def test_failed_dummy():
     assert False, 'shit happens'
-
-
-def test_read_orders():
-    df: pd.DataFrame = test_read_orders_df()
-    print(f'{df.shape=} got')
-    expected = (24, 5)
-    ass_mess = f'{df.shape =} is not correct, expected {expected}'
-    assert df.shape == expected, ass_mess
-    # 24 rows, 5 columns
-
-
-#  if __name__ == "__main__":    test_read_orders()
